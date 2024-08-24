7321a0447ab6e24a36dce9fc72c5f1ec5a1affc1
kacolter <kacolter@ualr.edu>
2024-08-24 18:03:14 +0000

    two timestampss

--- 01.06 Two Timestamps.py
+++ 01.06 Two Timestamps.py
@@ -1,8 +1,12 @@
 print("First Timestamp")
-hours= int(input("Number of Hours: "))
-minutes= int(input("Number of Minutes: "))
-seconds= int(input("Number of Seconds: "))
+hours1= int(input("Number of Hours: "))
+minutes1= int(input("Number of Minutes: "))
+seconds1= int(input("Number of Seconds: "))
 print("Second Timestamp")
-hours= int(input("Number of Hours: "))
-minutes= int(input("Number of Minutes: "))
-seconds= int(input("Number of Seconds: "))
+hours2= int(input("Number of Hours: "))
+minutes2= int(input("Number of Minutes: "))
+seconds2= int(input("Number of Seconds: "))
+total_seconds1 = hours1 * 3600 + minutes1 * 60 + seconds1
+total_seconds2 = hours2 * 3600 + minutes2 * 60 + seconds2
+time_difference = total_seconds2 - total_seconds1
+print(time_difference)
