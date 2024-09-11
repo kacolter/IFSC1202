6d92c4053033a66de0c236892be39dc73692a926
kacolter <kacolter@ualr.edu>
2024-09-11 14:41:41 +0000

    3.01

---  01.Project.py
+++  01.Project.py
@@ -1,4 +1,8 @@
 time=int(input("Enter Length of Time in Days: "))
-years=(time/365)
-weeks=(time / 7)
-days=(time - weeks)
+years=(time // 365)
+weeks=(time // 7)
+days=(time - weeks * 7)
+print(years)
+print(weeks)
+print(days)
+
