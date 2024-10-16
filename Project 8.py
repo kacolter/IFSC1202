119e5d20113e1cdfa19010e048d6a519128891e2
kacolter <kacolter@ualr.edu>
2024-10-16 18:40:33 +0000

    update

--- Project 8.py
+++ Project 8.py
@@ -1,18 +1,31 @@
-search = input("Enter Search Term: ")
 x = []
 constitution = open("constitution.txt", "r")
-i = constitution.readline()
-constitution_read = constitution.strip()
-for i in range(constitution_read + 1):
-    while i == search:
-        constitution.write()
-    if i == "":
-        x.append(i)
-        i = constitution.readline()
+line =  constitution.readline()
+while line != "":
+    x.append(line.strip())
+    line =  constitution.readline()
 constitution.close()
-for i in range(len(x)):
-    print(x[i])
+start = 0
+end = len(x)
 
+search = input("Enter Search Term: ")
+while search != "":
+    for i in range(len(x)):
+        if x[i].find(search) != -1:
+            start = 0
+            for j in range(i,0,-1):
+                if x[j] == "":
+                    start = j
+                    break
+            end = len(x)
+            for j in range(i,len(x)):
+                if x[j] == "":
+                    end = j
+                    break
 
+            for j in range(start,end+1):
+                print("Line",j,x[j])
+            print()
 
-
+        i = end + 1
+    search = input("Enter Search Term: ")
