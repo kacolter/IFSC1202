3520a8d435336b40fa8905a8ced46378397746ba
kacolter <kacolter@ualr.edu>
2024-10-22 21:19:17 +0000

    update

--- 9.01.py
+++ 9.01.py
@@ -1,24 +1,13 @@
-
 m, n = input("Enter the Number of Rows and Columns: ").split()
-m = 0
-n = 0
-
 a = []
 for i in range(int(m)):
     line = input("Enter a line of data: ").split()
     a.append(line)
-    maximum = max(a)
+    for j in range(len(a[i])):
+        max_column = max(a)
+        print(max_column)
+        print(a.index(max_column))
 print(a)
 
 
-for j in range(int(n[0])):
-        print(a.index(maximum))
-
-
-
-
-
-index = a.index(maximum)
-print(maximum)
-print(index)
 
--- 9.02.py
+++ 9.02.py
@@ -1,8 +1,21 @@
-a = []
 def print_list(a):
     file = numbers.readline()
-    file_split = file.split()
-a.append(file_split)
-print(a)
+    file_split = file.split(" ")
+    a.append(file_split)
 
+def swap_columns(a, i, j):
+    for column in a:
+        column[i], column[j] = column[j], column[i]
+
+def open_file(matrix):
+    with open(matrix, "r") as file:
+        return("9.02.txt".split())
+   
+matrix = "9.02.txt"
+a = open_file("9.02.txt")
+
+print_list(a)
+i, j = input("Enter the Columns to Swap: ")
+swap_columns(a, i, j)
+print_list(a)
 numbers = open("9.02.txt")
