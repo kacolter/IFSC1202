8202157776695ddf54ad923b6edc68089331a207
kacolter <kacolter@ualr.edu>
2024-10-23 14:15:19 +0000

    update

--- 9.02.py
+++ 9.02.py
@@ -1,21 +1,30 @@
-def print_list(a):
-    file = numbers.readline()
-    file_split = file.split(" ")
-    a.append(file_split)
-
 def swap_columns(a, i, j):
-    for column in a:
-        column[i], column[j] = column[j], column[i]
+    for k in range(len(a)):
+        a[k][i], a[k][j] = a[k][j], a[k][i]
+        #k = row a= number of rows; i and j are the columns to be swapped
+def print_array(a):
+    #loop through each row in the two dimensional array
+    for i in range(len(a)):
+        #loop through each element in list
+        for j in range(len(a[i])):
+            #print each eleemnt in list
+            print(a[i][j], end=' ')
+            #end of list- go to next line
+            print()
+            #create the array
+a = []
+numbersfile= open("9.02.txt")
+x = numbersfile.readline()
+while x!= "":
+    y = x.split(" ")
+    for i in range(len(y)):
+        y[i] = int(y[i])
+    a.append(y)
+    x = numbersfile.readline()
+print_array(a)
 
-def open_file(matrix):
-    with open(matrix, "r") as file:
-        return("9.02.txt".split())
-   
-matrix = "9.02.txt"
-a = open_file("9.02.txt")
-
-print_list(a)
-i, j = input("Enter the Columns to Swap: ")
+rowcolumnsline = input("Enter columns to swap: ").split()
+i = int(rowcolumnsline[0]) 
+j = int(rowcolumnsline[1])
 swap_columns(a, i, j)
-print_list(a)
-numbers = open("9.02.txt")
+print_array(a)
