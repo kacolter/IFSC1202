f6dbd4275240f580974fbb5fb6f473c6b8fe2367
kacolter <kacolter@ualr.edu>
2024-10-30 15:17:31 +0000

    update

--- 10 Pets.py
+++ 10 Pets.py
@@ -1,19 +1,21 @@
 class Pet ():
-    def _init_(self, name="Fido", type="Dog", age=3):
+    def __init__(self, name="Fido", type="Dog", age=3):
         self.Name = name
         self.Type = type
         self.Age = age
 filex = open("/workspaces/IFSC1202/10.01 Pets.txt")
 filex_read = filex.readline()
-while filex_read != "":
-    file_split = filex_read.split(",")
-    pet1 = Pet()
-    filex_read = filex.readline()
-    pet2 = Pet()
-    filex_read = filex.readline()
-    pet3 = Pet()
 
-print("Name".format(self.Name), "Type".format(self.Type), "Age".format(self.Age))
-print(pet1.Name, pet1.Type, pet1.Age())
-print(pet2.Name, pet2.Type, pet2.Age())
-print(pet3.Name, pet3.Type, pet3.Age())
+
+with open('10.01 Pets.txt', 'r') as file:
+    for line in file:
+        file_strip = line.strip().split(",")
+        file_read=file.readline()
+        pet1 = file_strip
+print(pet1)
+
+
+
+
+
+
