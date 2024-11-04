fe872b30a3b13000e638f070ce36ebc501e3adaf
kacolter <kacolter@ualr.edu>
2024-11-04 18:35:27 +0000

    update

--- 10.02.py
+++ 10.02.py
@@ -6,7 +6,26 @@ class RetailItem():
     def InventoryValue(self):
         inventoryvalue = self.UnitsOnHand * self.Price
         return inventoryvalue 
+    
 inven = open('10.02 Inventory.txt')
 x = inven.readline()
 y = x.split(",")
-myDescription1 = RetailItem()
+myDescription = RetailItem()
+myUnitsOnHand = RetailItem()
+myPrice = RetailItem() 
+myDescription.Description = y[0].strip()
+myUnitsOnHand.UnitsOnHand = y[1].strip()
+myPrice.Price = int(y[3].strip())
+x = inven.readline()
+y = x.split(",")
+myDescription.Description = y[0].strip()
+myUnitsOnHand.UnitsOnHand = y[1].strip()
+myPrice.Price = int(y[3].strip())
+x = inven.readline()
+y = x.split(",")
+myDescription.Description = y[0].strip()
+myUnitsOnHand.UnitsOnHand = y[1].strip()
+myPrice.Price = int(y[3].strip()) 
+inven.close()
+
+print()
